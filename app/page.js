53766c2cf170d34bcac407a9428d8568dd32c734
Ashutosh Raj Gupta <ag2364443@gmail.com>
2024-11-30 23:40:07 +0530

    project setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello,my name is ashu!! </h2>
+      <Button>Subscribe</Button>
+    </div>
+  );
+}
